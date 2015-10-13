54ffc1b4e40649dcb2c7f34c74e1e41f3f370aca
joshuagruenstein <joshuagruenstein@gmail.com>
2015-10-13 11:11:47 -0400

    up-down animation, cosmetics and weird trees

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@
 #define GOLDEN 1.61803398875
 #define DEGREE_TO_RADIAN 0.0174533
 
-#define LENGTH_FACTOR .75
-#define ANGLE_FACTOR 30
+#define LENGTH_FACTOR .8
+#define ANGLE_FACTOR 33
 #define THICK 2
 #define SEC_PER_FRAME 500
 
@@ -16,7 +16,7 @@ sf::Vector2i drawLine(sf::RenderWindow& window, int x, int y, int length, int th
     line.setPosition(x,window.getSize().y-y);
     line.rotate(theta);
     
-    line.setFillColor(sf::Color(0, 0, 0));
+    line.setFillColor(sf::Color(255, 255, 255));
     window.draw(line);
     
     sf::Vector2i returnVect(x+length*sin(theta*DEGREE_TO_RADIAN),
@@ -35,11 +35,22 @@ void drawTree(sf::RenderWindow& window, int x, int y, int theta,
     drawTree(window,newCoords.x,newCoords.y,theta - ANGLE_FACTOR/num,num+1,max,height*LENGTH_FACTOR);
 }
 
+void drawWeirdTree(sf::RenderWindow& window, int x, int y, int theta,
+                   int num, int max, int height) {
+    if (num > max) return;
+    
+    sf::Vector2i newCoords = drawLine(window,x,y,height,theta);
+    
+    drawWeirdTree(window,newCoords.x,newCoords.y,theta + ANGLE_FACTOR,num+1,max,height*LENGTH_FACTOR);
+    drawWeirdTree(window,newCoords.x,newCoords.y,theta - ANGLE_FACTOR,num+1,max,height*LENGTH_FACTOR);
+}
+
 int main(int, char const**) {
-    sf::RenderWindow window(sf::VideoMode(1300, 800), "Origins");
+    sf::RenderWindow window(sf::VideoMode(2000, 1500), "Origins", sf::Style::Fullscreen);
     
     sf::Clock clock;
     int layers;
+    bool weird;
     
     while (window.isOpen()) {
         
@@ -47,20 +58,30 @@ int main(int, char const**) {
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
                 window.close();
-            } if (event.type == sf::Event::KeyPressed &&
-                event.key.code == sf::Keyboard::Escape) {
-                window.close();
+            } if (event.type == sf::Event::KeyPressed) {
+                if (event.key.code == sf::Keyboard::Escape)
+                    window.close();
+                else if (event.key.code == sf::Keyboard::Up)
+                    layers++;
+                else if (event.key.code == sf::Keyboard::Down)
+                    layers--;
+                else if (event.key.code == sf::Keyboard::W)
+                    weird = true;
+                else if (event.key.code == sf::Keyboard::Q)
+                    weird = false;
             }
         }
 
-        window.clear(sf::Color(255,255,255));
+        window.clear(sf::Color(0,0,0));
 
+        /* ANIMATION
         if (clock.getElapsedTime().asMilliseconds() > SEC_PER_FRAME) {
             clock.restart();
             layers++;
-        }
+        }*/
         
-        drawTree(window,650,0,0,1,layers,200);
+        if (weird) drawWeirdTree(window,window.getSize().x/2,0,0,1,layers,350);
+        else drawTree(window,window.getSize().x/2,0,0,1,layers,350);
         
         window.display();
         
